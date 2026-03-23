6eef1e33756cbbf4508ac6ce876ee4859808a9bb
Star Power <dayan.marulanda@correounivalle.edu.co>
2026-03-23 16:22:10 -0500

    Creación de la clase Monstruo

---
+++ src/Monstruo.java
@@ -0,0 +1,37 @@
+public class Monstruo extends Carta {
+
+    private int atk;
+    private int def;
+    private int nivel;
+
+    public Monstruo(String nombre, int atk, int def, int nivel) {
+        super(nombre);
+        this.atk = atk;
+        this.def = def;
+        this.nivel = nivel;
+    }
+
+    public int getAtk() {
+        return atk;
+    }
+
+    public void setAtk(int atk) {
+        this.atk = atk;
+    }
+
+    public int getDef() {
+        return def;
+    }
+
+    public void setDef(int def) {
+        this.def = def;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+}
