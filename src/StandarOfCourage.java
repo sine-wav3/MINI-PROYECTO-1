f9afe1b95d2c6a5335d5a82f070205860e536518
unknown <juan.alejandro.marquez@correounivalle.edu.co>
2026-03-29 21:46:29 -0500

    Ultima magia que cree

---
+++ src/StandarOfCourage.java
@@ -0,0 +1,20 @@
+public class StandarOfCourage extends Magica {
+    
+    public StandarOfCourage(){
+        super("Standar Of Courage");
+    }
+
+    @Override
+    public void activar(Jugador jugador, Jugador oponente){
+        if(!jugador.getCampo().isEmpty()){
+            for (Monstruo m : jugador.getCampo()) {
+            m.setAtk(m.getAtk() + 200);
+        }
+        } else {
+            System.out.println("No tienes monstruos para aumentar su ataque");
+        }
+    }
+    
+    
+    
+}
