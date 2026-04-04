d66926404e42c5df6f096ff805d8b7e9198a41d4
unknown <juan.alejandro.marquez@correounivalle.edu.co>
2026-04-04 11:55:17 -0500

    ¡Mira! Una moneda

---
+++ src/TyphoonOfMagicalSpace.java
@@ -0,0 +1,24 @@
+public class TyphoonOfMagicalSpace extends Magica {
+
+    public TyphoonOfMagicalSpace() {
+        super("Typhoon Of Magical Space");
+    }
+
+    @Override
+    public void activar(Jugador jugador, Jugador oponente) {
+        if (oponente.getCampo().isEmpty()) {
+            System.out.println("No hay cartas para destruir");
+            return;
+        }
+        System.out.println("Elige un monstruo enemigo:");
+        for (int i = 0; i < oponente.getCampo().size(); i++) {
+            System.out.println(i + ". " + oponente.getCampo().get(i).getNombre());
+        }
+
+        int idx = sc.nextInt();
+
+        Monstruo eliminado = oponente.getCampo().remove(idx);
+
+        System.out.println("🌪️ Destruiste " + eliminado.getNombre());
+    }
+}
