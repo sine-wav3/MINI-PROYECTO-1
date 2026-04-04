be40ff35fe61001dfe4ac7f6df2f1c9df21fb0a3
unknown <juan.alejandro.marquez@correounivalle.edu.co>
2026-04-04 11:49:18 -0500

    Ahora pueden estar en modo ataque o modo defensa

--- src/Monstruo.java
+++ src/Monstruo.java
@@ -3,6 +3,8 @@ public class Monstruo extends Carta {
     private int atk;
     private int def;
     private int nivel;
+    private boolean enAtaque = true;
+    private boolean cambioEsteTurno = false;
 
     public Monstruo(String nombre, int atk, int def, int nivel) {
         super(nombre);
@@ -23,7 +25,38 @@ public class Monstruo extends Carta {
         return nivel;
     }
 
+    public boolean isEnAtaque() {
+        return enAtaque;
+    }
+
+    public boolean puedeCambiarModo() {
+        return !cambioEsteTurno;
+    }
+
     public void setAtk(int atk) {
         this.atk = atk;
+    }
+
+    public void setDef(int def){
+        this.def = def;
+    }
+
+    public void cambiarModo() {
+        if (!puedeCambiarModo()) {
+            System.out.println("Ya cambiaste el modo este turno");
+        return;
+    }
+    
+    enAtaque = !enAtaque;
+    marcarCambio();
+    System.out.println(getNombre() + " ahora está en " + (enAtaque ? "ataque" : "defensa"));
+    }
+
+    public void marcarCambio() {
+        cambioEsteTurno = true;
+    }
+    
+    public void resetCambio() {
+        cambioEsteTurno = false;
     }
 }
