190f3d9d8f49cea70bbf15af13407b0ca4a06783
sine-wav3 <daniela.franco.ibarra@correounivalle.edu.co>
2026-04-26 19:57:06 -0500

    se agrego la clase trampa para lo pedido en el mp2

---
+++ src/Trampa.java
@@ -0,0 +1,20 @@
+public abstract class Trampa extends Carta implements Activable {
+
+    private boolean activable = true;
+
+    public Trampa(String nombre) {
+        super(nombre);
+    }
+
+    public boolean isActivable() {
+        return activable;
+    }
+    
+    public void setActivable(boolean activable) {
+        this.activable = activable;
+    }
+    
+    // las trampas tienen una condicion para activarse, si se cumple, se activa, si no pos no
+    public abstract boolean condicion(Jugador jugador, Jugador oponente);
+    
+}
